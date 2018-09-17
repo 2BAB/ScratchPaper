21fbdf027bd51df3cfe32f79e57651be9db82721
2BAB <xx2bab@gmail.com>
2018-09-18 00:04:06 +0800

    update sample to display json file

--- sample/app/src/main/java/me/xx2bab/gradle/scratchpaper/sample/MainActivity.kt
+++ sample/app/src/main/java/me/xx2bab/gradle/scratchpaper/sample/MainActivity.kt
@@ -1,13 +1,24 @@
 package me.xx2bab.gradle.scratchpaper.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.method.ScrollingMovementMethod
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.json).apply {
+            text = loadScratchPaper()
+            movementMethod = ScrollingMovementMethod()
+        }
+    }
+
+    private fun loadScratchPaper(): String {
+        return assets.open("scratch-paper.json").bufferedReader().use { it.readText() }
     }
 
 }
