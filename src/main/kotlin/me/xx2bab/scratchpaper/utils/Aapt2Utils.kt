694dd6c27a6782b49ab28ba326075dc88174b58c
2BAB <xx2bab@gmail.com>
2018-09-30 21:56:40 +0800

    use object to replace class declaring

--- src/main/kotlin/me/xx2bab/scratchpaper/utils/Aapt2Utils.kt
+++ src/main/kotlin/me/xx2bab/scratchpaper/utils/Aapt2Utils.kt
@@ -4,28 +4,23 @@ import com.android.sdklib.BuildToolInfo
 import org.gradle.api.Project
 import java.io.File
 
-class Aapt2Utils {
+object Aapt2Utils {
 
-    companion object {
+    fun compileResDir(project: Project, targetDir: File, resFiles: List<File>) {
+        val androidPluginUtils = AndroidPluginUtils(project)
+        val androidBuilder = androidPluginUtils.getAndroidBuilder()
+        val aapt2ExecutorPath = androidBuilder?.buildToolInfo?.getPath(BuildToolInfo.PathId.AAPT2)
 
-        fun compileResDir(project: Project, targetDir: File, resFiles: List<File>) {
-            val androidPluginUtils = AndroidPluginUtils(project)
-            val androidBuilder = androidPluginUtils.getAndroidBuilder()
-            val aapt2ExecutorPath = androidBuilder?.buildToolInfo?.getPath(BuildToolInfo.PathId.AAPT2)
-
-            project.exec { execSpec ->
-                execSpec.executable(aapt2ExecutorPath)
-                execSpec.args("compile")
-                execSpec.args("--legacy")
-                execSpec.args("-o")
-                execSpec.args(targetDir.absolutePath)
-                resFiles.forEach {
-                    execSpec.args(it.absolutePath)
-                }
+        project.exec { execSpec ->
+            execSpec.executable(aapt2ExecutorPath)
+            execSpec.args("compile")
+            execSpec.args("--legacy")
+            execSpec.args("-o")
+            execSpec.args(targetDir.absolutePath)
+            resFiles.forEach {
+                execSpec.args(it.absolutePath)
             }
-
         }
-
     }
 
 }
--- src/main/kotlin/me/xx2bab/scratchpaper/utils/Logger.kt
+++ src/main/kotlin/me/xx2bab/scratchpaper/utils/Logger.kt
@@ -3,30 +3,27 @@ package me.xx2bab.scratchpaper.utils
 import org.gradle.api.Project
 import org.gradle.api.logging.Logger
 
-class Logger {
+object Logger {
 
-    companion object {
+    private const val TAG = "[ScratchPaper]: "
 
-        private const val TAG = "[ScratchPaper]: "
+    private lateinit var logUtil: Logger
 
-        private var logUtil: Logger? = null
-
-        fun init(project: Project) {
-            logUtil = project.logger
-        }
-
-        fun d(message: String) {
-            logUtil?.debug(TAG + message)
-        }
-
-        fun i(message: String) {
-            logUtil?.info(TAG + message)
-        }
-
-        fun e(message: String) {
-            logUtil?.error(TAG + message)
-        }
-
+    fun init (project: Project){
+        logUtil = project.logger
     }
+
+    fun d(message: String) {
+        logUtil.debug(TAG + message)
+    }
+
+    fun i(message: String) {
+        logUtil.info(TAG + message)
+    }
+
+    fun e(message: String) {
+        logUtil.error(TAG + message)
+    }
+
 
 }
