49d0f729c36d34b6fb7c343a6e882032e9df8dde
2BAB <xx2bab@gmail.com>
2023-12-28 15:36:22 +0800

    fix: no more javadocJar required, it's done by java{withJavadocJar()}

--- buildSrc/src/main/kotlin/maven-central-publish.gradle.kts
+++ buildSrc/src/main/kotlin/maven-central-publish.gradle.kts
@@ -29,10 +29,6 @@ if (secretPropsFile.exists()) {
     ext["ossrh.username"] = System.getenv("OSSRH_USERNAME")
     ext["ossrh.password"] = System.getenv("OSSRH_PASSWORD")
 }
-val javadocJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-}
-
 fun getExtraString(name: String) = ext[name]?.toString()
 
 
@@ -74,12 +70,9 @@ publishing {
     }
 }
 
-signing {
-    sign(publishing.publications)
-}
-
 afterEvaluate {
     publishing.publications.all {
+        signing.sign(this)
         val publicationName = this.name
         (this as MavenPublication).apply {
             if (publicationName == "pluginMaven") {
@@ -87,7 +80,6 @@ afterEvaluate {
                 artifactId = projectName
             }
             version = BuildConfig.Versions.scratchPaperVersion
-            artifact(javadocJar.get())
 
             pom {
                 if (publicationName == "pluginMaven") {
